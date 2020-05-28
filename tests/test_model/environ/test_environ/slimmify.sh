83cdadce408643c367784916421fbaecab2f79cd
Richard Seifert <seifertricharda@gmail.com>
2020-05-28 17:11:27 -0400

    Removed old testing infrastructure

--- tests/test_model/environ/test_environ/slimmify.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ls 1environ.* | awk '{if (NR%2==0) if (substr($1,length($1))=="*") print substr($1,1,length($1)-1); else print $1}' > rmfiles.txt
-xargs rm < rmfiles.txt
-rm -f rmfiles.txt
