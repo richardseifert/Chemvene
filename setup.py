9672edf86d86321a70e6730df252df6807c52867
Richard Seifert <seifertricharda@gmail.com>
2020-08-25 14:29:37 -0500

    Merged read functions into single file

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='chemvene',
-      version='1.1',
+      version='1.2',
       description='A convenient chemical model visualization and analysis engine for processing output from the Michigan protoplanetary disk chemical modeling code.',
       url='https://github.com/richardseifert/Chemvene',
       author='Richard Seifert',
       author_email='seifertricharda@gmail.com',
       license='MIT',
-      packages=['chemvene','chemvene.read','chemvene.pkg_files'],
+      packages=['chemvene','chemvene.pkg_files'],
       install_requires=[
             'matplotlib',
             'numpy',
